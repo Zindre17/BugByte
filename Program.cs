bff98054ac9698adfaea219235422fb6f4f01c12
Sindre Aarstrand <zindre17@gmail.com>
2023-05-20 20:27:00 +0200

    Extract lexing into function

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Diagnostics;
-
-if (args.Length == 0)
+﻿if (args.Length == 0)
 {
     Console.WriteLine("Please provide a file name to lex");
     return;
@@ -13,40 +11,41 @@ if (args.Length > 1)
 
 var fileName = args[0];
 
-var stopWatch = new Stopwatch();
-stopWatch.Start();
-var lines = File.ReadAllLines(fileName);
-Console.WriteLine($"Read {lines.Length} lines in {stopWatch.ElapsedMilliseconds}ms");
-stopWatch.Restart();
-
-var words = new List<Token>();
-var lineNr = 1;
-foreach (var line in lines)
-{
-    var remainigLine = line.TrimStart();
-    var currentColumn = line.Length - remainigLine.Length + 1; // index starts at 1
-    while (remainigLine.Length > 0)
-    {
-        var split = remainigLine.Split(' ', 2);
-        words.Add(new Token(split[0], lineNr, currentColumn));
-        if (split.Length > 1)
-        {
-            currentColumn += split[0].Length + 1; // +1 for the space
-            remainigLine = split[1].TrimStart();
-            currentColumn += split[1].Length - remainigLine.Length;
-        }
-        else
-        {
-            remainigLine = "";
-        }
-    }
-    lineNr++;
-}
-Console.WriteLine($"Processed {lines.Length} lines in {stopWatch.ElapsedMilliseconds}ms");
+var words = LexProgram(fileName);
 
 foreach (var word in words)
 {
     Console.WriteLine($"{fileName}:{word.Line}:{word.Column} - {word.Value}");
+}
+
+
+List<Token> LexProgram(string filename)
+{
+    var lines = File.ReadAllLines(filename);
+    var words = new List<Token>();
+    var lineNr = 1;
+    foreach (var line in lines)
+    {
+        var remainigLine = line.TrimStart();
+        var currentColumn = line.Length - remainigLine.Length + 1; // index starts at 1
+        while (remainigLine.Length > 0)
+        {
+            var split = remainigLine.Split(' ', 2);
+            words.Add(new Token(split[0], lineNr, currentColumn));
+            if (split.Length > 1)
+            {
+                currentColumn += split[0].Length + 1; // +1 for the space
+                remainigLine = split[1].TrimStart();
+                currentColumn += split[1].Length - remainigLine.Length;
+            }
+            else
+            {
+                remainigLine = "";
+            }
+        }
+        lineNr++;
+    }
+    return words;
 }
 
 record Token(string Value, int Line, int Column);
