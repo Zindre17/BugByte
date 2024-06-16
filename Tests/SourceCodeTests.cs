ea68a3acdac3617495e6530de3e8392f89f3b119
Sindre Aarstrand <saarstrand@akvagroup.com>
2024-06-16 22:38:44 +0200

    Added SourceCode abstraction

---
+++ Tests/SourceCodeTests.cs
@@ -0,0 +1,183 @@
+namespace Tests;
+
+[TestClass]
+public class SourceCodeTests
+{
+    private readonly Token firstToken = Token.OnlyValue("a");
+    private readonly Token secondToken = Token.OnlyValue("b");
+    private readonly Token thirdToken = Token.OnlyValue("c");
+
+    [TestMethod]
+    public void HasNextToken_InitializedNonEmptySourceCode_True()
+    {
+        var sourceCode = new SourceCode([firstToken]);
+
+        Assert.IsTrue(sourceCode.HasNextToken());
+    }
+
+    [TestMethod]
+    public void HasNextToken_NonEmptySourcePartiallyConsumed_True()
+    {
+        var SourceCode = new SourceCode([firstToken, secondToken, thirdToken]);
+
+        SourceCode.MoveNext();
+        SourceCode.MoveNext();
+
+        Assert.IsTrue(SourceCode.HasNextToken());
+    }
+
+    [TestMethod]
+    public void HasNextToken_NonEmptySourceCodeFullyConsumed_False()
+    {
+        var SourceCode = new SourceCode([firstToken, secondToken]);
+
+        SourceCode.MoveNext();
+        SourceCode.MoveNext();
+
+        Assert.IsFalse(SourceCode.HasNextToken());
+    }
+
+
+    [TestMethod]
+    public void HasNextToken_EmptySourceCode_False()
+    {
+        var SourceCode = new SourceCode([firstToken]);
+
+        Assert.IsTrue(SourceCode.HasNextToken());
+    }
+
+    [TestMethod]
+    public void MoveNext_InitializedNonEmptySourceCode_FirstToken()
+    {
+        var SourceCode = new SourceCode([firstToken]);
+
+        Assert.AreEqual(firstToken, SourceCode.MoveNext());
+    }
+
+    [TestMethod]
+    public void MoveNext_EmptySourceCode_ThrowsException()
+    {
+        var SourceCode = new SourceCode([]);
+
+        Assert.ThrowsException<EndOfCodeException>(SourceCode.MoveNext);
+    }
+
+    [TestMethod]
+    public void MoveNext_InitializedNonEmptySourceCode_CanBeCalledTheSameAmountOfTimesAsElements()
+    {
+        var SourceCode = new SourceCode([firstToken, secondToken, thirdToken]);
+
+        SourceCode.MoveNext();
+        SourceCode.MoveNext();
+        SourceCode.MoveNext();
+
+        Assert.ThrowsException<EndOfCodeException>(SourceCode.MoveNext);
+    }
+
+    [TestMethod]
+    public void MoveNext_InitializedNonEmptySourceCode_OrderOfTokensIsPreserved()
+    {
+        var SourceCode = new SourceCode([firstToken, secondToken, thirdToken]);
+
+        Assert.AreEqual(firstToken, SourceCode.MoveNext());
+        Assert.AreEqual(secondToken, SourceCode.MoveNext());
+        Assert.AreEqual(thirdToken, SourceCode.MoveNext());
+    }
+
+    [TestMethod]
+    public void PeekNextToken_EmptySourceCode_ThrowsException()
+    {
+        var SourceCode = new SourceCode([]);
+
+        Assert.ThrowsException<EndOfCodeException>(SourceCode.PeekNextToken);
+    }
+
+    [TestMethod]
+    public void PeekNextToken_NonEmptySourceCode_FirstToken()
+    {
+        var SourceCode = new SourceCode([firstToken]);
+
+        Assert.AreEqual(firstToken, SourceCode.PeekNextToken());
+    }
+
+    [TestMethod]
+    public void PeekNextToken_NonEmptySourceCode_CanBeCalledMultipleTimes()
+    {
+        var SourceCode = new SourceCode([firstToken, secondToken, thirdToken]);
+
+        Assert.AreEqual(firstToken, SourceCode.PeekNextToken());
+        Assert.AreEqual(firstToken, SourceCode.PeekNextToken());
+        Assert.AreEqual(firstToken, SourceCode.PeekNextToken());
+    }
+
+    [TestMethod]
+    public void CurrentToken_EmptySourceCode_ThrowsException()
+    {
+        var SourceCode = new SourceCode([]);
+
+        Assert.ThrowsException<NoCurrentTokenYetException>(SourceCode.CurrentToken);
+    }
+
+    [TestMethod]
+    public void CurrentToken_NonEmptySourceCodeNotYetMovedToNext_ThrowsException()
+    {
+        var SourceCode = new SourceCode([firstToken]);
+
+        Assert.ThrowsException<NoCurrentTokenYetException>(SourceCode.CurrentToken);
+    }
+
+    [TestMethod]
+    public void CurrentToken_NonEmptySourceCodeAfterMoveNext_FirstToken()
+    {
+        var SourceCode = new SourceCode([firstToken]);
+
+        SourceCode.MoveNext();
+
+        Assert.AreEqual(firstToken, SourceCode.CurrentToken());
+    }
+
+    [TestMethod]
+    public void HasRemainingTokens_NonEmptySourceCode_EnoughTokens()
+    {
+        var SourceCode = new SourceCode([firstToken, secondToken, thirdToken]);
+
+        Assert.IsTrue(SourceCode.HasRemainingTokens(2));
+    }
+
+    [TestMethod]
+    public void HasRemainingTokens_NonEmptySourceCode_NotEnoughTokens()
+    {
+        var SourceCode = new SourceCode([firstToken, secondToken, thirdToken]);
+
+        Assert.IsFalse(SourceCode.HasRemainingTokens(4));
+    }
+
+    [TestMethod]
+    public void HasRemainingTokens_NonEmptySourceCode_ExactAmountOfTokens()
+    {
+        var SourceCode = new SourceCode([firstToken, secondToken, thirdToken]);
+
+        Assert.IsTrue(SourceCode.HasRemainingTokens(3));
+    }
+
+    [TestMethod]
+    public void HasRemainingTokens_NonEmptySourceCode_ExactAmountOfTokensAfterMoveNext()
+    {
+        var SourceCode = new SourceCode([firstToken, secondToken, thirdToken]);
+
+        SourceCode.MoveNext();
+
+        Assert.IsTrue(SourceCode.HasRemainingTokens(2));
+    }
+
+    [TestMethod]
+    public void HasRemainingTokens_NonEmptySourceCode_ExactAmountOfTokensAfterSkipNextToken()
+    {
+        var SourceCode = new SourceCode([firstToken, secondToken, thirdToken]);
+
+        SourceCode.MoveNext();
+
+        Assert.IsTrue(SourceCode.HasRemainingTokens(2));
+    }
+}
+
---
+++ src/SourceCode.cs
@@ -0,0 +1,36 @@
+namespace BugByte;
+
+internal class SourceCode
+{
+    private int currentPosition = 0;
+    private readonly Token[] tokens;
+
+    public SourceCode(Token[] tokens) => this.tokens = tokens;
+    public SourceCode(IEnumerable<Token> tokens) => this.tokens = tokens.ToArray();
+
+    public bool HasNextToken() => HasRemainingTokens(1);
+    public Token MoveNext() => HasNextToken() ? tokens[currentPosition++] : throw new EndOfCodeException();
+    public Token PeekNextToken() => HasNextToken() ? tokens[currentPosition] : throw new EndOfCodeException();
+
+    internal Token CurrentToken() => currentPosition is 0 ? throw new NoCurrentTokenYetException() : tokens[currentPosition - 1];
+
+    internal bool HasRemainingTokens(int count)
+    {
+        if (count <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(count), "Count must be greater than 0");
+        }
+
+        return currentPosition + (count - 1) < tokens.Length;
+    }
+}
+
+internal class EndOfCodeException : Exception
+{
+    public EndOfCodeException() : base("End of file reached") { }
+}
+
+internal class NoCurrentTokenYetException : Exception
+{
+    public NoCurrentTokenYetException() : base("No token in focus yet") { }
+}
